5cfcc1e49c7abe43d84e12f915b146191390d840
dssalt <dssalt@yandex.com>
2021-01-29 01:02:23 -0500

    Added codemirror.keymap.js to root dir

---
+++ codemirror.keymap.js
@@ -0,0 +1,8 @@
+CodeMirror.Vim.map('<C-j>', '}')
+CodeMirror.Vim.map('<C-k>', '{')
+CodeMirror.Vim.map('<C-h>', 'b')
+CodeMirror.Vim.map('<C-l>', 'e')
+
+CodeMirror.Vim.map('<C-i>', '<Esc><Right>', 'insert')
+
+document.querySelector('.CodeMirror+.CodeMirror').style.display='none'
