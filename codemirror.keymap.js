8425b5b42ea0a8da2b30c3b7348649d2a979134c
dssalt <dssalt@yandex.com>
2021-01-29 05:28:13 -0500

    Updated codemirror.keymap.js

--- codemirror.keymap.js
+++ codemirror.keymap.js
@@ -2,7 +2,16 @@ CodeMirror.Vim.map('<C-j>', '}')
 CodeMirror.Vim.map('<C-k>', '{')
 CodeMirror.Vim.map('<C-h>', 'b')
 CodeMirror.Vim.map('<C-l>', 'e')
-
 CodeMirror.Vim.map('<C-i>', '<Esc><Right>', 'insert')
 
+window.cm = document.querySelector('.CodeMirror').CodeMirror
+
 document.querySelector('.CodeMirror+.CodeMirror').style.display='none'
+
+document.querySelector('article').style.maxWidth='unset'
+document.querySelector('article').style.width='90%'
+document.querySelector('article').style.margin='0 5%'
+
+document.querySelectorAll('.darkreader').forEach(a=>a.remove())
+
+console.log('CodeMirror keys loaded')
